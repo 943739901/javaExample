558872c2777e42fa78b3ce3b14dde5e5d4559298
lipengyu <lipengyu@shencom.cn>
2021-06-17 18:45:31 +0800

    commit

---
+++ src/main/java/com/lpy/test/base/AMAP/TestAmap.java
@@ -0,0 +1,14 @@
+package com.lpy.test.base.AMAP;
+
+/**
+ * @author lipengyu
+ */
+public class TestAmap {
+
+    public static void main(String[] args) {
+//        DistanceSearch distanceSearch = new DistanceSearch();
+//
+//
+//        distanceSearch.ca();
+    }
+}
